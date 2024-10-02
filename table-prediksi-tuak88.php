c052afdeac91b0c640c003f65ee68dd0cdf605c2
pastibisanaikinstall <153497022+pastibisanaikinstall@users.noreply.github.com>
2024-10-02 12:57:43 +0700

    Create table-prediksi-tuak88.php

---
+++ table-prediksi-tuak88.php
@@ -0,0 +1,326 @@
+    <div class="entry-ld-container">
+        <table style="border-collapse: collapse; width: 100%; height: 208px; background-color:#EBEBEB;">
+            <tbody>
+                            	            <h2 class="prediksi" style="align: center;">PREDIKSI SINGAPORE 02 OKTOBER 2024</h2>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 10px;">BBFS</td>
+                                <td style="width: 48.2877%; height: 10px;">
+                                    6*1*7*5*8+8                                </td>
+                            </tr> 
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas</td>
+                                <td id="content-togelBebas" style="width: 48.2877%; height: 22px;">
+                                    9                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas 2D</td>
+                                <td id="content-togelBebas2d" style="width: 48.2877%; height: 22px;">
+                                    9 & 2                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Dasar</td>
+                                <td id="content-dasar" style="width: 48.2877%; height: 22px;">
+                                    Besar & Ganjil                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Shio</td>
+                                <td id="content-shio" style="width: 48.2877%; height: 22px;">
+                                    Babi                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;"> 2D BOM</td>
+                                <td id="content-bom2d" style="width: 48.2877%; height: 22px;">
+                                    32*98*69*93*59*92*25*89*94*23                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Jaga Twin</td>
+                                <td id="content-twin" style="width: 48.2877%; height: 22px;">
+                                    55*99*22*77                                </td>
+                            </tr>
+                              
+            </tbody>
+        </table>
+    </div>
+    <div class="entry-ld-container">
+        <table style="border-collapse: collapse; width: 100%; height: 208px; background-color:#EBEBEB;">
+            <tbody>
+                            	            <h2 class="prediksi" style="align: center;">PREDIKSI HONGKONG 02 OKTOBER 2024</h2>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 10px;">BBFS</td>
+                                <td style="width: 48.2877%; height: 10px;">
+                                    7*3*2*6*8+0                                </td>
+                            </tr> 
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas</td>
+                                <td id="content-togelBebas" style="width: 48.2877%; height: 22px;">
+                                    3                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas 2D</td>
+                                <td id="content-togelBebas2d" style="width: 48.2877%; height: 22px;">
+                                    8 & 3                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Dasar</td>
+                                <td id="content-dasar" style="width: 48.2877%; height: 22px;">
+                                    Besar & Ganjil                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Shio</td>
+                                <td id="content-shio" style="width: 48.2877%; height: 22px;">
+                                    Macan                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;"> 2D BOM</td>
+                                <td id="content-bom2d" style="width: 48.2877%; height: 22px;">
+                                    38*72*63*86*97*83*24*88*67*76                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Jaga Twin</td>
+                                <td id="content-twin" style="width: 48.2877%; height: 22px;">
+                                    71*91*19*36                                </td>
+                            </tr>
+                              
+            </tbody>
+        </table>
+    </div>
+    <div class="entry-ld-container">
+        <table style="border-collapse: collapse; width: 100%; height: 208px; background-color:#EBEBEB;">
+            <tbody>
+                            	            <h2 class="prediksi" style="align: center;">PREDIKSI SYDNEY 02 OKTOBER 2024</h2>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 10px;">BBFS</td>
+                                <td style="width: 48.2877%; height: 10px;">
+                                    8*4*6*2*9+0                                </td>
+                            </tr> 
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas</td>
+                                <td id="content-togelBebas" style="width: 48.2877%; height: 22px;">
+                                    7                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas 2D</td>
+                                <td id="content-togelBebas2d" style="width: 48.2877%; height: 22px;">
+                                    7 & 4                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Dasar</td>
+                                <td id="content-dasar" style="width: 48.2877%; height: 22px;">
+                                    Besar & Ganjil                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Shio</td>
+                                <td id="content-shio" style="width: 48.2877%; height: 22px;">
+                                    Tikus                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;"> 2D BOM</td>
+                                <td id="content-bom2d" style="width: 48.2877%; height: 22px;">
+                                    46*81*74*32*79*91*87*92*39*41                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Jaga Twin</td>
+                                <td id="content-twin" style="width: 48.2877%; height: 22px;">
+                                    81*91*37*61                                </td>
+                            </tr>
+                              
+            </tbody>
+        </table>
+    </div>
+    <div class="entry-ld-container">
+        <table style="border-collapse: collapse; width: 100%; height: 208px; background-color:#EBEBEB;">
+            <tbody>
+                            	            <h2 class="prediksi" style="align: center;">PREDIKSI MAGNUM4D 02 OKTOBER 2024</h2>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 10px;">BBFS</td>
+                                <td style="width: 48.2877%; height: 10px;">
+                                    9*8*3*7*4+0                                </td>
+                            </tr> 
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas</td>
+                                <td id="content-togelBebas" style="width: 48.2877%; height: 22px;">
+                                    10                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas 2D</td>
+                                <td id="content-togelBebas2d" style="width: 48.2877%; height: 22px;">
+                                    6 & 7                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Dasar</td>
+                                <td id="content-dasar" style="width: 48.2877%; height: 22px;">
+                                    Besar & Ganjil                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Shio</td>
+                                <td id="content-shio" style="width: 48.2877%; height: 22px;">
+                                    Naga                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;"> 2D BOM</td>
+                                <td id="content-bom2d" style="width: 48.2877%; height: 22px;">
+                                    12*23*45*65*59*67*25*78*54*09                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Jaga Twin</td>
+                                <td id="content-twin" style="width: 48.2877%; height: 22px;">
+                                    12*54*67*78                                </td>
+                            </tr>
+                              
+            </tbody>
+        </table>
+    </div>
+    <div class="entry-ld-container">
+        <table style="border-collapse: collapse; width: 100%; height: 208px; background-color:#EBEBEB;">
+            <tbody>
+                            	            <h2 class="prediksi" style="align: center;">PREDIKSI PCSO 02 OKTOBER 2024</h2>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 10px;">BBFS</td>
+                                <td style="width: 48.2877%; height: 10px;">
+                                    4*1*3*5*6+7                                </td>
+                            </tr> 
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas</td>
+                                <td id="content-togelBebas" style="width: 48.2877%; height: 22px;">
+                                    9                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas 2D</td>
+                                <td id="content-togelBebas2d" style="width: 48.2877%; height: 22px;">
+                                    9 & 4                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Dasar</td>
+                                <td id="content-dasar" style="width: 48.2877%; height: 22px;">
+                                    Besar & Ganjil                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Shio</td>
+                                <td id="content-shio" style="width: 48.2877%; height: 22px;">
+                                    Kerbau                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;"> 2D BOM</td>
+                                <td id="content-bom2d" style="width: 48.2877%; height: 22px;">
+                                    41*87*62*52*74*49*36*49*94*49                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Jaga Twin</td>
+                                <td id="content-twin" style="width: 48.2877%; height: 22px;">
+                                    53*49*82*41                                </td>
+                            </tr>
+                              
+            </tbody>
+        </table>
+    </div>
+    <div class="entry-ld-container">
+        <table style="border-collapse: collapse; width: 100%; height: 208px; background-color:#EBEBEB;">
+            <tbody>
+                            	            <h2 class="prediksi" style="align: center;">PREDIKSI DAMACAI4D 02 OKTOBER 2024</h2>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 10px;">BBFS</td>
+                                <td style="width: 48.2877%; height: 10px;">
+                                    5*3*9*8*4+0                                </td>
+                            </tr> 
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas</td>
+                                <td id="content-togelBebas" style="width: 48.2877%; height: 22px;">
+                                    17                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Colok Bebas 2D</td>
+                                <td id="content-togelBebas2d" style="width: 48.2877%; height: 22px;">
+                                    1 & 7                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Dasar</td>
+                                <td id="content-dasar" style="width: 48.2877%; height: 22px;">
+                                    Besar & Ganjil                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Shio</td>
+                                <td id="content-shio" style="width: 48.2877%; height: 22px;">
+                                    Kelinci                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;"> 2D BOM</td>
+                                <td id="content-bom2d" style="width: 48.2877%; height: 22px;">
+                                    12*45*23*35*67*78*56*34*23*57                                </td>
+                            </tr>
+                            <tr style="height: 25px;">
+                                <td style="width: 35%; height: 22px;">Jaga Twin</td>
+                                <td id="content-twin" style="width: 48.2877%; height: 22px;">
+                                    21*23*34*54                                </td>
+                            </tr>
+                              
+            </tbody>
+        </table>
+    </div>
+</body>
+<style>
+    .prediksi {
+        background-color: #0007ab;
+        color: white;
+        padding:10px 0;
+    }
+    .entry-ld-container {
+        width: 100%;
+        text-align: center;
+        margin: auto;
+        border: 8px solid #EBEBEB;
+        /* padding: 2.5px; */
+        border-radius: .25rem;
+    }
+    
+    .entry-ld-container td {
+        width: 15%;
+        border: 1px solid #EBEBEB;
+        /* padding: 10px 0; */
+        text-align: center;
+        font-size: 12px;
+        letter-spacing: 1px;
+        height: 50px;
+    }
+    
+    .entry-ld-container table {
+        width: 100%;
+        border-collapse: collapse;
+        margin: auto;
+        border-spacing: 0;
+    }
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    
+    @media and screen (min-width: 500px) {
+        table { 
+            table-layout:fixed;
+            overflow-x: auto;
+            display: block;
+        },
+        
+        td { 
+            overflow: hidden; 
+            text-overflow: ellipsis; 
+            word-wrap: break-word;
+        }
+        
+        .entry-ld-container td {
+            font-size: 10px;
+            height: 45px;
+        }
+        
+        table, html, body {
+            width: 100%;
+        }
+        
+        .entry-ld-container td {
+            font-size: 10px;
+            height: 70px;
+        }
+    }
+</style>
+</html>
